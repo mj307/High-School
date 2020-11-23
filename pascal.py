775f7dd14c2fe35ce0f46d49cf70172ab8354a72
Medhavi <Medhavi@Medhavis-MacBook-Pro.local>
2020-11-23 11:01:17 -0800

    pascal game

---
+++ pascal.py
@@ -0,0 +1,30 @@
+
+def pascal(curr,row,p_list):
+    if row == curr:
+        print p_list
+        return True
+    else:
+        l_pas = [1]
+        if curr == 0:
+            p_list.append(l_pas)
+            pascal(curr+1,row,p_list)
+        elif curr == 1:
+            l_pas.append(1)
+            p_list.append(l_pas)
+            pascal(curr + 1, row, p_list)
+        else:
+            x = p_list[-1]
+            for a in range(0,len(x)-1):
+                l_pas.append(p_list[-1][a] + p_list[-1][a+1])
+            l_pas.append(1)
+            p_list.append(l_pas)
+            pascal(curr + 1, row, p_list)
+
+p_list = []
+pascal(0,6,p_list)
+
+
+
+
+
+
